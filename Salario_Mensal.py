658507f95d3834c5383fe072fe107b72dcd2e00f
Degoracy <138266258+Degoracy@users.noreply.github.com>
2023-08-30 08:14:26 -0300

    Códigos.

---
+++ Salario_Mensal.py
@@ -0,0 +1,5 @@
+valor_hora = float(input("Quanto você ganha por hora: "))
+horas_trabalhadas = float(input("Quantas horas você trabalha por mês: "))
+
+salario = valor_hora * horas_trabalhadas
+print("Seu salario é de ", salario)
