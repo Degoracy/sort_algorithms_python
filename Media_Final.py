abec1815679a48e15e14ab15744b74a42e724d51
Degoracy <138266258+Degoracy@users.noreply.github.com>
2023-08-30 08:09:42 -0300

    Códigos.

---
+++ Media_Final.py
@@ -0,0 +1,7 @@
+n1 = float(input("Informe a primeira nota: "))
+n2 = float(input("Informe a segunda nota: "))
+n3 = float(input("Informe a terceira nota: "))
+n4 = float(input("Informe a quarta nota: "))
+
+media = (n1 + n2 + n3 + n4) / 4.
+print("Sua média é ", media)
