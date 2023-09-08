7e8d911e389ebd20d838f8cda43f08e09d4bb400
Degoracy <138266258+Degoracy@users.noreply.github.com>
2023-09-08 20:33:23 -0300

    Códigos.

---
+++ src/merge_sort.py
@@ -0,0 +1,18 @@
+def sort(array):
+    sort_half(array, 0, len(array) - 1)
+
+
+def sort_half(array, start, end):
+    if start >= end:
+        return
+
+    middle = (start + end) // 2
+
+    sort_half(array, start, middle)
+    sort_half(array, middle + 1, end)
+
+    merge(array, start, end)
+
+
+def merge(array, start, end):
+    array[start: end + 1] = sorted(array[start: end + 1])
