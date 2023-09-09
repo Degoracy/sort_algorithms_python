6a60e4b2c432a8f337745d826f70035d8e830f65
Degoracy <138266258+Degoracy@users.noreply.github.com>
2023-09-08 21:14:33 -0300

    Códigos.

---
+++ loop.py
@@ -0,0 +1,8 @@
+numero = int(input("Informe um número entre 1,10: "))
+
+while (numero <1) or (numero >10):
+    print('Só número entre 1 e 10 meu jovem.')
+    numero = int(input("Informe um número entre 1,10: "))
+
+else:
+    print('Seja Bem-Vindo.')
